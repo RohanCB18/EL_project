7fdf2831a0b61e2292354faba3e6f30d57d4aa7b
Rohan Bharadwaj <rohan.blr21@gmail.com>
2026-01-18 16:06:24 +0530

    style: switch to Geist font family (official npm package)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import 'geist/style/sans.css'
+import 'geist/style/mono.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
